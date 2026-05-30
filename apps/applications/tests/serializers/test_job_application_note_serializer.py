034abe7e29b1c19f3699fa273e2eb9fe66f34e2a
Behrooz <behrouzmaasser1@gmail.com>
2026-05-30 03:13:29 +0200

    Write serializer tests applications app

---
+++ apps/applications/tests/serializers/test_job_application_note_serializer.py
@@ -0,0 +1,121 @@
+from apps.applications.api.serializers import JobApplicationNoteSerializer
+
+
+class TestJobApplicationNoteSerializer:
+
+    def test_valid_data(self):
+
+        data = {
+            "title": "Interview feedback",
+            "content": "Strong technical discussion.",
+        }
+
+        serializer = JobApplicationNoteSerializer(data=data)
+
+        assert serializer.is_valid(), serializer.errors
+
+    def test_rejects_blank_title(self):
+
+        data = {
+            "title": "",
+            "content": "Some content",
+        }
+
+        serializer = JobApplicationNoteSerializer(data=data)
+
+        assert not serializer.is_valid()
+        assert "title" in serializer.errors
+
+    def test_rejects_null_title(self):
+
+        data = {
+            "title": None,
+            "content": "Some content",
+        }
+
+        serializer = JobApplicationNoteSerializer(data=data)
+
+        assert not serializer.is_valid()
+        assert "title" in serializer.errors
+
+    def test_rejects_title_too_long(self):
+
+        data = {
+            "title": "a" * 61,
+            "content": "Some content",
+        }
+
+        serializer = JobApplicationNoteSerializer(data=data)
+
+        assert not serializer.is_valid()
+        assert "title" in serializer.errors
+
+    def test_rejects_blank_content(self):
+
+        data = {
+            "title": "Valid title",
+            "content": "",
+        }
+
+        serializer = JobApplicationNoteSerializer(data=data)
+
+        assert not serializer.is_valid()
+        assert "content" in serializer.errors
+
+    def test_rejects_null_content(self):
+
+        data = {
+            "title": "Valid title",
+            "content": None,
+        }
+
+        serializer = JobApplicationNoteSerializer(data=data)
+
+        assert not serializer.is_valid()
+        assert "content" in serializer.errors
+
+    def test_read_only_job_application_ignored(self):
+
+        data = {
+            "title": "Test",
+            "content": "Test content",
+            "job_application": 99999999,
+        }
+
+        serializer = JobApplicationNoteSerializer(data=data)
+
+        assert serializer.is_valid(), serializer.errors
+        assert "job_application" not in serializer.validated_data
+
+    def test_requires_title(self):
+
+        data = {
+            "content": "Some content",
+        }
+
+        serializer = JobApplicationNoteSerializer(data=data)
+
+        assert not serializer.is_valid()
+        assert "title" in serializer.errors
+
+    def test_requires_content(self):
+
+        data = {
+            "title": "Valid title",
+        }
+
+        serializer = JobApplicationNoteSerializer(data=data)
+
+        assert not serializer.is_valid()
+        assert "content" in serializer.errors
+
+    def test_accepts_title_at_max_length(self):
+
+        data = {
+            "title": "a" * 60,
+            "content": "Valid content",
+        }
+
+        serializer = JobApplicationNoteSerializer(data=data)
+
+        assert serializer.is_valid(), serializer.errors
---
+++ apps/applications/tests/serializers/test_job_application_serializer.py
@@ -0,0 +1,126 @@
+from django.utils import timezone
+
+from apps.applications.api.serializers import JobApplicationSerializer
+from apps.documents.tests.conftest import doc1_user1, document_type_user1, fake_file1
+
+
+class TestJobApplicationSerializer:
+
+    def test_valid_data(self, status1, email1_co1_ws1_user1, doc1_user1):
+
+        data = {
+            "status": status1.pk,
+            "date_applied": timezone.now(),
+            "emails": [email1_co1_ws1_user1.id],
+            "documents": [doc1_user1.id],
+        }
+
+        serializer = JobApplicationSerializer(data=data)
+
+        assert serializer.is_valid(), serializer.errors
+
+    def test_valid_without_optional_fields(self, status1):
+
+        data = {
+            "status": status1.pk,
+        }
+
+        serializer = JobApplicationSerializer(data=data)
+
+        assert serializer.is_valid(), serializer.errors
+
+    def test_accepts_null_date_applied(self, status1):
+
+        data = {
+            "status": status1.pk,
+            "date_applied": None,
+        }
+
+        serializer = JobApplicationSerializer(data=data)
+
+        assert serializer.is_valid(), serializer.errors
+
+    def test_rejects_invalid_email_pk(self, status1):
+
+        data = {
+            "status": status1.pk,
+            "emails": [999999999999],
+        }
+
+        serializer = JobApplicationSerializer(data=data)
+
+        assert not serializer.is_valid()
+        assert "emails" in serializer.errors
+
+    def test_rejects_invalid_document_pk(self, status1):
+
+        data = {
+            "status": status1.pk,
+            "documents": [999999],
+        }
+
+        serializer = JobApplicationSerializer(data=data)
+
+        assert not serializer.is_valid()
+        assert "documents" in serializer.errors
+
+    def test_rejects_invalid_date_applied(self, status1):
+
+        data = {
+            "status": status1.pk,
+            "date_applied": "not-a-date",
+        }
+
+        serializer = JobApplicationSerializer(data=data)
+
+        assert not serializer.is_valid()
+        assert "date_applied" in serializer.errors
+
+    def test_read_only_fields_are_ignored(self, status1):
+
+        data = {
+            "status": status1.pk,
+            "owner": 999,
+            "workspace": 999,
+            "job_position": 999,
+        }
+
+        serializer = JobApplicationSerializer(data=data)
+
+        assert serializer.is_valid(), serializer.errors
+        assert "owner" not in serializer.validated_data
+        assert "workspace" not in serializer.validated_data
+        assert "job_position" not in serializer.validated_data
+
+    def test_rejects_invalid_status(self):
+
+        data = {
+            "status": "invalid_status"
+        }
+
+        serializer = JobApplicationSerializer(data=data)
+
+        assert not serializer.is_valid()
+        assert "status" in serializer.errors
+
+    def test_accepts_empty_emails(self, status1):
+
+        data = {
+            "status": status1.pk,
+            "emails": [],
+        }
+
+        serializer = JobApplicationSerializer(data=data)
+
+        assert serializer.is_valid(), serializer.errors
+
+    def test_accepts_empty_documents(self, status1):
+
+        data = {
+            "status": status1.pk,
+            "documents": [],
+        }
+
+        serializer = JobApplicationSerializer(data=data)
+
+        assert serializer.is_valid(), serializer.errors
