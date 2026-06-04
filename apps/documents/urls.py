d93fb08335ae91f519e3711e049c6636d24198f8
Behrooz <behrouzmaasser1@gmail.com>
2026-06-04 20:56:17 +0200

    Write Document web views in documents. Add App url paths

--- apps/documents/urls.py
+++ apps/documents/urls.py
@@ -1,14 +1,35 @@
 from django.urls import path
 
 from apps.documents.views import (
-    DocumentTypeListView, DocumentTypeCreateView, DocumentTypeDetailView, DocumentTypeDeleteView, DocumentTypeUpdateView
+    # Document Type Views
+    DocumentTypeListView,
+    DocumentTypeCreateView,
+    DocumentTypeDetailView,
+    DocumentTypeDeleteView,
+    DocumentTypeUpdateView,
+
+    # Document Views
+    DocumentListView,
+    DocumentCreateView,
+    DocumentDetailView,
+    DocumentUpdateView,
+    DocumentDeleteView,
+    DownloadDocumentView,
+    OpenDocumentView,
 
 )
 
 urlpatterns = [
+    path("documents/<int:pk>/download/", DownloadDocumentView.as_view(), name="document-download-web"),
+    path("documents/<int:pk>/open/", OpenDocumentView.as_view(), name="document-open-web"),
     path("document_types/", DocumentTypeListView.as_view(), name="document-type-list-web"),
     path("document_types/create/", DocumentTypeCreateView.as_view(), name="document-type-create-web"),
     path("document_types/<int:pk>/", DocumentTypeDetailView.as_view(), name="document-type-detail-web"),
     path("document_types/<int:pk>/edit/", DocumentTypeUpdateView.as_view(), name="document-type-edit-web"),
     path("document_types/<int:pk>/delete/", DocumentTypeDeleteView.as_view(),name="document-type-delete-web"),
+    path("documents/", DocumentListView.as_view(), name="document-list-web"),
+    path("documents/create/", DocumentCreateView.as_view(), name="document-create-web"),
+    path("documents/<int:pk>/", DocumentDetailView.as_view(), name="document-detail-web"),
+    path("documents/<int:pk>/edit/", DocumentUpdateView.as_view(), name="document-edit-web"),
+    path("documents/<int:pk>/delete/", DocumentDeleteView.as_view(),name="document-delete-web"),
 ]
--- apps/documents/views.py
+++ apps/documents/views.py
@@ -1,6 +1,11 @@
+from mimetypes import guess_type
+
 from django.contrib.auth.mixins import LoginRequiredMixin
-from django.shortcuts import redirect
+from django.http import FileResponse
+from django.shortcuts import redirect, get_object_or_404
 from django.urls import reverse, reverse_lazy
+from django.utils.text import slugify
+from django.views import View
 from django.views.generic import (
     ListView,
     CreateView,
@@ -10,10 +15,12 @@ from django.views.generic import (
 )
 
 # Models
-from apps.documents.models import DocumentType
+from apps.documents.models import DocumentType, Document
+from apps.documents.selectors.document_selector import DocumentSelector
 
 # Selectors
 from apps.documents.selectors.document_type_selector import DocumentTypeSelector
+from apps.documents.services.document_service import DocumentService
 
 # Services
 from apps.documents.services.document_type_service import DocumentTypeService
@@ -114,3 +121,130 @@ class DocumentTypeDeleteView(LoginRequiredMixin, DeleteView):
         )
 
         return redirect("document-type-list-web")
+
+
+class DocumentListView(LoginRequiredMixin, ListView):
+
+    model = Document
+    template_name = "documents/document/list.html"
+    context_object_name = "documents"
+
+    def get_queryset(self):
+
+        return DocumentSelector.list(user=self.request.user)
+
+
+class DocumentCreateView(LoginRequiredMixin, CreateView):
+
+    model = Document
+    template_name = "documents/document/create.html"
+    fields = ["name", "document_type", "file"]
+
+    def form_valid(self, form):
+
+        DocumentService.create(
+            user=self.request.user,
+            validated_data=form.cleaned_data
+        )
+
+        return redirect(self.get_success_url())
+
+    def form_invalid(self, form):
+
+        return super().form_invalid(form)
+
+    def get_success_url(self):
+
+        return reverse_lazy("document-list-web")
+
+
+class DocumentDetailView(LoginRequiredMixin, DetailView):
+
+    model = Document
+    template_name = "documents/document/detail.html"
+    context_object_name = "document"
+
+    def get_queryset(self):
+
+        return DocumentSelector.list(user=self.request.user)
+
+
+class DocumentUpdateView(LoginRequiredMixin, UpdateView):
+
+    model = Document
+    template_name = "documents/document_type/edit.html"
+    fields = ["name", "description"]
+
+    def get_queryset(self):
+
+        return DocumentSelector.list(user=self.request.user)
+
+    def form_valid(self, form):
+
+        DocumentService.update(
+            user=self.request.user,
+            document_id=self.kwargs["pk"],
+            validated_data=form.cleaned_data
+        )
+
+        return redirect(self.get_success_url())
+
+    def get_success_url(self):
+
+        return reverse(
+            "document-detail-web",
+            kwargs={"pk": self.kwargs["pk"]}
+        )
+
+    def form_invalid(self, form):
+
+        return super().form_invalid(form)
+
+
+class DocumentDeleteView(LoginRequiredMixin, DeleteView):
+
+    model = Document
+    template_name = "documents/document/delete.html"
+
+    def get_queryset(self):
+
+        return DocumentSelector.list(user=self.request.user)
+
+    def post(self, request, *args, **kwargs):
+
+        DocumentService.remove(
+            user=self.request.user,
+            document_id=self.kwargs["pk"],
+        )
+
+        return redirect("document-list-web")
+
+
+class BaseDocumentFileView(LoginRequiredMixin, View):
+
+    as_attachment = False
+
+    def get(self, request, *args, **kwargs):
+
+        document = get_object_or_404(
+            Document, owner=request.user, pk=self.kwargs["pk"]
+        )
+
+        content_type, _ = guess_type(document.file.name)
+
+        return FileResponse(
+            document.file.open("rb"),
+            as_attachment=self.as_attachment,
+            filename=slugify(document.name),
+            content_type=content_type,
+        )
+
+
+class DownloadDocumentView(BaseDocumentFileView):
+
+    as_attachment = True
+
+
+class OpenDocumentView(BaseDocumentFileView):
+
+    as_attachment = False
