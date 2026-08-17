0818fe891c96f2009c1a25150e44baad6cca0007
Behrooz <behrouzmaasser1@gmail.com>
2026-08-17 10:04:19 +0200

    fix(download document view bug): download view was returning a file without extension, which is resolved now.

--- apps/documents/mixins/document_file_response_mixin.py
+++ apps/documents/mixins/document_file_response_mixin.py
@@ -1,4 +1,5 @@
 from mimetypes import guess_type
+from pathlib import Path
 
 from django.http import FileResponse
 from django.utils.text import slugify
@@ -15,12 +16,14 @@ class DocumentFileResponseMixin:
     def get_response(self):
 
         document = self.get_document()
+        doc_name = document.name.replace(' ', '_')
+        extension = Path(document.file.name).suffix
 
         content_type, _ = guess_type(document.file.name)
 
         return FileResponse(
             document.file.open("rb"),
             as_attachment=self.as_attachment,
-            filename=slugify(document.name),
+            filename=doc_name + extension,
             content_type=content_type,
         )
